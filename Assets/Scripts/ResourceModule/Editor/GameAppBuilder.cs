83e5dd26d88563a0d3a1f126ec6d30709bb0ba85
linkabox <linkabox@foxmail.com>
2018-12-17 19:20:39 +0800

    Add GameAppBuilder

---
+++ Assets/Scripts/ResourceModule/Editor/GameAppBuilder.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Build;
+using UnityEngine;
+
+public class GameAppBuilder
+{
+	[MenuItem("ResourceModule/BuildGame")]
+	public static void BuildGame()
+	{
+		BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+		var buildScenes = new List<string>();
+		var allScenes = EditorBuildSettings.scenes;
+		foreach (var scene in allScenes)
+		{
+			if (scene.enabled)
+			{
+				var importer = AssetImporter.GetAtPath(scene.path);
+				if (importer != null && string.IsNullOrEmpty(importer.assetBundleName))
+				{
+					buildScenes.Add(scene.path);
+				}
+			}
+		}
+
+		buildPlayerOptions.scenes = buildScenes.ToArray();
+		string path = EditorUtility.OpenFilePanel("BuildApp", "", "");
+		buildPlayerOptions.locationPathName = path;
+		buildPlayerOptions.target = EditorUserBuildSettings.activeBuildTarget;
+		buildPlayerOptions.options = BuildOptions.None;
+		BuildPipeline.BuildPlayer(buildPlayerOptions);
+	}
+}
